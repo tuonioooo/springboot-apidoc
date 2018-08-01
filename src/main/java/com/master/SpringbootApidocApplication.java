c0cd91c1badc2f79ec44e3a38d769a766177f3c3
tuonioooo <daizhaoman@sina.com>
2018-08-01 16:17:07 +0800

    修改成war包启动方式
    修改成war包启动方式

--- src/main/java/com/master/SpringbootApidocApplication.java
+++ src/main/java/com/master/SpringbootApidocApplication.java
@@ -3,11 +3,22 @@ package com.master;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.io.IOException;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-public class SpringbootApidocApplication {
+public class SpringbootApidocApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApidocApplication.class, args);
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(SpringbootApidocApplication.class);
 	}
 }
